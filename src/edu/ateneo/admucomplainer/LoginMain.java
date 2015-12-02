299eded0dfb9a9c9828f5b97eebfd73eaffa547a
Mikko Luis Saavedra <vovo27_miranemiko@yahoo.co.jp>
2015-12-02 22:18:44 +0800

    Lots of things were added

--- src/edu/ateneo/admucomplainer/LoginMain.java
+++ src/edu/ateneo/admucomplainer/LoginMain.java
@@ -1,11 +1,17 @@
 package edu.ateneo.admucomplainer;
 
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginMain extends Activity {
 
@@ -37,5 +43,55 @@ public class LoginMain extends Activity {
 	{
 		Intent intent = new Intent(getApplicationContext(), Register.class);
 		startActivity(intent);
+	}
+	public void signIn(View v)
+	{
+		EditText uN = (EditText) findViewById(R.id.userName);
+		EditText pW = (EditText) findViewById(R.id.password);
+		String unStr = uN.getText().toString();
+		String pwStr = pW.getText().toString();
+		barOnButts(true);
+		ParseUser.logInInBackground(unStr, pwStr, new LogInCallback() {
+			public void done(ParseUser user, ParseException e) {
+			if (user != null) 
+				{
+				// Hooray! The user is logged in.
+				// NOTE: Intent below should lead us to complainer home screen
+				//Intent intent = new Intent(getApplicationContext(), WelcomeActivity.class);
+				//startActivity(intent);
+				Toast.makeText(getApplicationContext(), "Sign in successful!\nNow fix me!", Toast.LENGTH_LONG).show();
+				} 
+			else 
+				{
+				// Signup failed. Look at the ParseException to see what happened.
+				e.printStackTrace();
+				Toast.makeText(getApplicationContext(), "Unable to sign in: "+e.getMessage(), Toast.LENGTH_LONG).show();
+				
+				}
+			
+			
+			// enable the buttons and disable the spinner
+			barOnButts(false);
+			
+			}
+		});	
+	}
+	private void barOnButts(Boolean chk)
+	{
+		View butt1 = findViewById(R.id.signIn);
+		View butt2 = findViewById(R.id.register);
+		View spin = findViewById(R.id.progressBar1);
+		
+		if (chk == true){
+			butt1.setEnabled(false);
+			butt2.setEnabled(false);
+			spin.setVisibility(View.VISIBLE);
+		}
+		else if (chk == false){
+			butt1.setEnabled(true);
+			butt2.setEnabled(true);
+			spin.setVisibility(View.INVISIBLE);
+		}
+
 	}
 }
